7a713cec89456b255ae3c57c4f1242d47dba6929
林綉琳 <0529hsiu@gmail.com>
2025-05-15 13:20:23 +0000

    0513

--- lesson6/lesson6_1.py
+++ lesson6/lesson6_1.py
@@ -5,6 +5,20 @@ def calculate_bmi(height:int,weight:int):
             raise Exception("體重不在偵測範圍")
     bmi=weight/height**2
     return bmi
+def get_state(bmi:float)->str:
+    if  bmi<18.5:
+        message="體重過輕"
+    elif bmi<=24:
+        message="體中適中"
+    elif bmi<=27:
+        message="輕度肥胖"
+    elif bmi<=35:
+        message="中度肥胖"
+    else:
+        message='重度肥胖'
+    return message
+    
+
 def main():
     try:
         height=float(input("請輸入身高(公尺)"))
@@ -18,16 +32,7 @@ def main():
         print(f"你的身高:{height}")
         print(f"你的體重:{weight}") 
         print(f"你的bmi:{bmi:.2f}")
-    if  bmi<18.5:
-        print("體重過輕")
-    elif bmi<=24:
-        print("體中適中")
-    elif bmi<=27:
-        print("輕度肥胖")
-    elif bmi<=35:
-        print("中度肥胖")
-    else:
-        print("重度肥胖")
+    print(get_state(bmi))
             
 
 
